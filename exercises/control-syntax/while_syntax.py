13c9ef17e2d45af71ef587cfc6bd422ae808a81b
cehl-kurage <yusakuishihira@gmail.com>
2023-03-09 17:54:39 +0900

    Fix while assignment

--- exercises/control-syntax/while_syntax.py
+++ exercises/control-syntax/while_syntax.py
@@ -1,17 +1,9 @@
-# 関数は x**2とする
-min_x, max_x = map(float, input().split())  # 定義域の与え方どうしよう
-width_list = (0.1, 0.01, 0.001, 0.0001)
-total_area_list = []  # 近似精度があがっていくのを確認する用
-for width in width_list:
-    x = min_x
-    total_area = 0
+# ユークリッドの互除法
+a, b = map(int, input().split())
 
-    while x < max_x:
-        left_height = x**2
-        right_height = (x + width) ** 2
-        total_area += (left_height + right_height) * width / 2
-        x += width
-
-    total_area_list.append(total_area)
-
-print(total_area_list)
+r = a % b
+while r != 0:
+    a = b
+    b = r
+    r = a % b
+print(f"{b} が最大公約数")
