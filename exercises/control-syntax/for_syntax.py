5d96efdd03c31bc5952b8a1a6fe06118ed598017
cehl-kurage <yusakuishihira@gmail.com>
2023-03-09 15:07:42 +0900

    add assignments for control syntax

---
+++ exercises/control-syntax/for_syntax.py
@@ -0,0 +1,14 @@
+# nが素数か判定せよ。
+n = int(input())
+max_num = int(n ** (1/2)) # floor
+
+is_prime = True
+for m in range(2, max_num + 1):
+    r = n % m
+    if r == 0:
+        is_prime = False
+        break
+if is_prime:
+    print("素数")
+else:
+    print("素数じゃない")
